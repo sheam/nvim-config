b1d1e2f1dd9e0b571bb03313a17fc05fce01cbd4
Shea Martin <shea.martin@gmail.com>
2026-01-17 13:39:25 -0400

    fix missing dependancy for yazi plugin

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,4 +1,7 @@
-vim.pack.add({'https://github.com/mikavilpas/yazi.nvim'})
+vim.pack.add({
+	'https://github.com/nvim-lua/plenary.nvim',
+	'https://github.com/mikavilpas/yazi.nvim',
+})
 require('yazi').setup({
 	open_for_directories = true,
 	keymaps = {
